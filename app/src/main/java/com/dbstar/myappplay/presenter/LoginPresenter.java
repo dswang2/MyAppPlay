56372a0d46c941170ff84856ff6a9d777b70dda7
ame <465616185@qq.com>
2017-10-09 17:40:07 +0800

    基本完成到K31，实现登录与token的保存，登录成功后可以利用RxBus通知导航栏更新头像

--- app/src/main/java/com/dbstar/myappplay/presenter/LoginPresenter.java
+++ app/src/main/java/com/dbstar/myappplay/presenter/LoginPresenter.java
@@ -1,5 +1,7 @@
 package com.dbstar.myappplay.presenter;
 
+import android.util.Log;
+
 import com.dbstar.myappplay.bean.LoginBean;
 import com.dbstar.myappplay.common.rx.RxHttpReponseCompat;
 import com.dbstar.myappplay.common.util.ACache;
@@ -7,6 +9,7 @@ import com.dbstar.myappplay.common.util.Constant;
 import com.dbstar.myappplay.common.util.LogUtils;
 import com.dbstar.myappplay.common.util.VerificationUtils;
 import com.dbstar.myappplay.presenter.contract.LoginContract;
+import com.hwangjr.rxbus.RxBus;
 
 import rx.Subscriber;
 
@@ -25,9 +28,9 @@ public class LoginPresenter extends BasePresenter<LoginContract.ILoginModel, Log
         LogUtils.e("dswang", "LoginPresenter.login.email = " + email);
         LogUtils.e("dswang", "LoginPresenter.login.password = " + password);
 
-        if(VerificationUtils.matcherPhoneNum(email)){
+        if (VerificationUtils.matcherPhoneNum(email)) {
             mView.checkPhoneSuccess();
-        }else {
+        } else {
             mView.checkPhoneError();
             return;
         }
@@ -36,29 +39,39 @@ public class LoginPresenter extends BasePresenter<LoginContract.ILoginModel, Log
         mModel.login(email, password)
                 .compose(RxHttpReponseCompat.<LoginBean>compatResult())
                 .subscribe(new Subscriber<LoginBean>() {
-            @Override
-            public void onCompleted() {
-                LogUtils.e("dswang", "LoginPresenter.onCompleted. = ");
-            }
 
-            @Override
-            public void onError(Throwable e) {
-                LogUtils.e("dswang", "LoginPresenter.onError. = " + e.getStackTrace() + e.toString());
-            }
+                    @Override
+                    public void onStart() {
+                        Log.e("dswang","LoginPresenter.onStart(LoginPresenter.java:43)");
+                        mView.showLoading();
+                    }
 
-            @Override
-            public void onNext(LoginBean loginBean) {
-                LogUtils.e("dswang", "LoginPresenter.onNext. = " + loginBean.toString());
-                saveUser(loginBean);
-                // 利用RxBus 通知侧滑导航栏的头部更新登陆成功的头像
-                
-            }
-        });
+                    @Override
+                    public void onCompleted() {
+                        LogUtils.e("dswang", "LoginPresenter.onCompleted. = ");
+                        mView.dismissLoading();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        LogUtils.e("dswang", "LoginPresenter.onError. = " + e.getStackTrace() + e.toString());
+                        mView.dismissLoading();
+                    }
+
+                    @Override
+                    public void onNext(LoginBean loginBean) {
+                        LogUtils.e("dswang", "LoginPresenter.onNext. = " + loginBean.toString());
+                        mView.loginSuccess(loginBean);
+                        saveUser(loginBean);
+                        // 利用RxBus 通知侧滑导航栏的头部更新登陆成功的头像
+                        RxBus.get().post(loginBean.getUser());
+                    }
+                });
     }
 
     private void saveUser(LoginBean loginBean) {
         ACache aCache = ACache.get(mContext);
-        aCache.put(Constant.USER,loginBean.getUser());
-        aCache.put(Constant.TOKEN,loginBean.getToken());
+        aCache.put(Constant.USER, loginBean.getUser());
+        aCache.put(Constant.TOKEN, loginBean.getToken());
     }
 }
--- app/src/main/java/com/dbstar/myappplay/ui/activity/MainActivity.java
+++ app/src/main/java/com/dbstar/myappplay/ui/activity/MainActivity.java
@@ -9,13 +9,20 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.dbstar.myappplay.R;
+import com.dbstar.myappplay.bean.User;
+import com.dbstar.myappplay.common.imageloader.GlideCircleTransform;
 import com.dbstar.myappplay.common.util.ToastUtils;
 import com.dbstar.myappplay.di.component.AppComponent;
 import com.dbstar.myappplay.ui.adapter.ViewPagerAdapter;
+import com.hwangjr.rxbus.RxBus;
+import com.hwangjr.rxbus.annotation.Subscribe;
 
 import butterknife.BindView;
 
@@ -32,10 +39,15 @@ public class MainActivity extends BaseActivity {
     @BindView(R.id.main_view_pager)
     ViewPager mainViewPager;
     private View headerView;
+    private ImageView header_iv;
 
 
     @Override
     protected void init() {
+
+        // 注册RxBus
+        RxBus.get().register(this);
+
         // 初始化侧滑菜单
         initDrawLayout();
 
@@ -48,14 +60,23 @@ public class MainActivity extends BaseActivity {
         initUser();
     }
 
+    @Subscribe
+    public void getUser(User user){
+        Log.e("dswang","MainActivity.getUser(MainActivity.java:60)"+user.toString());
+        // 利用user更新头像
+        // .transform(new GlideCircleTransform(this)), glide 加载圆形ImageView图像
+        Glide.with(this).load(user.getLogo_url()).transform(new GlideCircleTransform(this)).into(header_iv);
+    }
+
     private void initUser() {
         headerView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,LoginActivity.class));
-                mainDrawer.closeDrawers();
+                //mainDrawer.closeDrawers();
             }
         });
+
     }
 
     @Override
@@ -107,6 +128,9 @@ public class MainActivity extends BaseActivity {
     private void initDrawLayout() {
         // 侧滑菜单头部点击监听
         headerView = mainNav.getHeaderView(0);
+        // 用户头像
+        header_iv = (ImageView) headerView.findViewById(R.id.header_iv);
+
         // 侧滑菜单菜单点击监听
         mainNav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -114,31 +138,31 @@ public class MainActivity extends BaseActivity {
                 switch (item.getItemId()) {
                     case R.id.main_nav_menu_appAndGame:
                         ToastUtils.showSafeToast(MainActivity.this, "应用和游戏");
-                        mainDrawer.closeDrawers();
+                        //mainDrawer.closeDrawers();
                         break;
                     case R.id.main_nav_menu_download:
                         ToastUtils.showSafeToast(MainActivity.this, "下载");
-                        mainDrawer.closeDrawers();
+                        //mainDrawer.closeDrawers();
                         break;
                     case R.id.main_nav_menu_update:
                         ToastUtils.showSafeToast(MainActivity.this, "更新");
-                        mainDrawer.closeDrawers();
+                        //mainDrawer.closeDrawers();
                         break;
                     case R.id.main_nav_menu_message:
                         ToastUtils.showSafeToast(MainActivity.this, "消息");
-                        mainDrawer.closeDrawers();
+                        //mainDrawer.closeDrawers();
                         break;
                     case R.id.main_nav_menu_setting:
                         ToastUtils.showSafeToast(MainActivity.this, "设置");
-                        mainDrawer.closeDrawers();
+                        //mainDrawer.closeDrawers();
                         break;
                     case R.id.main_nav_menu_help:
                         ToastUtils.showSafeToast(MainActivity.this, "帮助和反馈");
-                        mainDrawer.closeDrawers();
+                        //mainDrawer.closeDrawers();
                         break;
                     case R.id.main_nav_menu_about:
                         ToastUtils.showSafeToast(MainActivity.this, "关于");
-                        mainDrawer.closeDrawers();
+                        //mainDrawer.closeDrawers();
                         break;
                 }
                 return false;
@@ -148,4 +172,10 @@ public class MainActivity extends BaseActivity {
 
     }
 
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        RxBus.get().unregister(this);
+    }
 }
